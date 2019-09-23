93d268ddb895cec3f30f3389faab945d9853fac7
Ben <bigriffin94@outlook.com>
2019-09-22 22:58:37 -0700

    Login Form, SignUp Form, axiosWithAuth; General rendering tests; node-sass files created and ready to use.

---
+++ advice-giver-fe-arch/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import './App.css';
+
+// Components
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Hello From APP</h1>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
