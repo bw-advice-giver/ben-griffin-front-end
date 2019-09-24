5983e38f38df142537889726eda0c7790f3dd91c
Hector-bit <hectormartinez.2978@gmail.com>
2019-09-24 16:31:29 -0700

    cardslist and card details are done || styling added to 2 components

--- advice-giver/advice-giver-fe-arch/src/components/QuestionsCard/QuestionsCard.js
+++ advice-giver/advice-giver-fe-arch/src/components/QuestionsCard/QuestionsCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Stylediv from './Stylediv';
+import { Link } from 'react-router-dom';
+import Box from './Styleh2';
 
 const QuestionsCard = props => {
-    const { title, question } = props;
+
+    const { title, question, user } = props
+    
     return (
-        <div className="question-card">
-        <h2>Title: {title}</h2>
-        <p>Question: {question}</p>
+        <div>
+        <Stylediv className="one-card">
+            <div>
+            <h2>Title: {title}</h2>
+            <p>Question: {question}</p>
+            </div>
+         </Stylediv>
         </div>
     )
 }
+
 
 export default QuestionsCard;
---
+++ advice-giver/advice-giver-fe-arch/src/components/QuestionsCard/Styleh2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styleh2 = styled.h2`
+  border-radius: 3px;
+  `
+
+export default Styleh2;
