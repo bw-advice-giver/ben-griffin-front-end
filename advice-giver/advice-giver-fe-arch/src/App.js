97c3b87047bc05efb34f532db89696a374ccc13a
Rayven <rayven.burns@gmail.com>
2019-09-27 12:16:37 -0400

    another update to app.js

--- advice-giver/advice-giver-fe-arch/src/App.js
+++ advice-giver/advice-giver-fe-arch/src/App.js
@@ -55,7 +55,7 @@ function App() {
           <Route exact path = "/advisor-signup" component = {SignUpAdvi} /> 
           <PrivateRoute exact path="/profile" component={Profile}/>
           {/* <PrivateRoute exact path="/questions" component={QuestionsList}/> */}
-          <Route exact path="/addquestion" component={FormikAddQuestion}/> 
+          <PrivateRoute exact path="/addquestion" component={FormikAddQuestion}/> 
 
         </Switch>
       </div>
